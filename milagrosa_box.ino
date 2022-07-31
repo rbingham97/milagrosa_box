c0722566c4207b80487cfbb50b5edc85955869ca
atrujillo98 <110239824+atrujillo98@users.noreply.github.com>
2022-07-30 22:49:34 -0400

    Update milagrosa_box.ino

--- milagrosa_box.ino
+++ milagrosa_box.ino
@@ -1,33 +1,38 @@
 // code for comms boxes at La Milagrosa
-// Russell Bingham
-// 7/27/22
+// Russell Bingham & Aaron Trujillo
+// 7/30/22
 
 int num_buttons = 5;
+int message_play = 11; // assuming pin 13 is available -- used for message record/send/play
+int message_available = 13; // assuming 14 is available
+int message_receive = 15; // an output for the light when a message comes in
 int input_pins[5] = {2, 4, 7, 8, 12}; // switches
 int output_pins[5] = {3, 5, 6, 9, 10}; // lights (PWM)
-int states[5] = {0, 0, 0, 0, 0}; // FSM states
 int brightnesses[5] = {0, 0, 0, 0, 0}; // light states
-int directions[5] = {1, 1, 1, 1, 1}; // up/down blinking
-// b if that button has been pressed, reset to a on send
-char staging[5] = {'a', 'a', 'a', 'a', 'a'}; // part of debouncing
-int last_val[5] = {0, 0, 0, 0, 0}; // what the button was last cycle
-char press_ID [5] = {'1', '2', '3', '4', '5'};
-char incoming_notif = '0';
+
+byte message[100];
 
 int max_brightness = 100; 
+int current_state = 0;
 
-int num_states = 4; // off, solid, blinking, fading -- in order
-int slopes[4] = {0, 0, 3, 3};
-int offsets[4] = {0, max_brightness, 0, max_brightness};
-int execute_offset[4] = {1, 1, 0, 0};
+int totalBytesInMessage = 0;
 
-int holder = 0;
-
-void update_state(int button, int state) {
-  states[button] = state;
-  if (execute_offset[state] == 1) {
-    brightnesses[button] = offsets[state];
+byte recordMessage() {
+  byte output;
+  for (int i = 0; i < num_buttons; i++) {
+    output |= digitalRead(input_pins[i]) << i;
   }
+  delay(100);
+  return output;
+}
+
+void playMessage(byte lightSequence) {
+  int brightness;
+  for (int i = 0; i < num_buttons; i++) {
+    brightness = lightSequence >> i ? brightnesses[i] : 0;
+    analogWrite(output_pins[i], brightness);
+  }
+  delay(100);
 }
 
 void setup() {
@@ -37,66 +42,63 @@ void setup() {
     pinMode(input_pins[i], INPUT);
     pinMode(output_pins[i], OUTPUT);
   }
+  pinMode(message_play, INPUT);
+  pinMode(message_available, OUTPUT);
 }
 
 void loop() {
-
-  // read any press_IDs from remote
-  if (Serial.available() > 0) {
-    incoming_notif = Serial.read();
-    for (int i = 0; i < num_buttons; i++) {
-      if (press_ID[i] == incoming_notif) {
-        if (states[i] == 0) {
-          update_state(i, 2); // off to blinking
-        }
-        else if (states[i] == 1) {
-          update_state(i, 3); // solid to fading
-        }      
+  switch (current_state) {
+    case 0: // base state
+      // check for incoming message
+      if (Serial.available() > 0) {
+        totalBytesInMessage = Serial.read();
+        // turn on light, indicating message has been received
+        digitalWrite(message_available, HIGH);
+        current_state = 3;
         break;
       }
-    }
-  }
-
-  // check for pressed buttons locally
-  for (int i = 0; i < num_buttons; i++) {
-    holder = digitalRead(input_pins[i]);
-    if (holder == HIGH && last_val[i] != HIGH) {
-      staging[i] = 'b';
-      if (states[i] == 0) { // off to solid
-        update_state(i, 1);
+      // check to record message
+      if (digitalRead(message_play)) {
+        current_state = 1;
       }
-      else if (states[i] == 2) { // blinking to off
-        update_state(i, 3);
+      break;
+    case 1: // record and send message
+      for (int i = 0; i < 100; ++i) {
+        totalBytesInMessage = i;
+        // check for message completion
+        if (digitalRead(message_play)) {
+          current_state = 2;
+          break;
+        }
+        message[i] = recordMessage();
       }
-    }
-    last_val[i] = holder;
-  }
-
-  // update lights
-  for (int i = 0; i < num_buttons; i++) {
-    if (brightnesses[i] >= max_brightness) {
-      directions[i] = -1;
-    }
-    else if (brightnesses[i] <= slopes[i]) {
-      directions[i] = 1;
-      if (states[i] == 3) {
-        update_state(i, 0);
+      Serial.write(totalBytesInMessage);
+      current_state = 2;
+      break;
+    case 2: // message delivery
+      Serial.write(0xFF);
+      Serial.write(message, totalBytesInMessage);
+      // only one message can be sent at a time. To avoid multiple sends,
+      // sender waits until receiver plays the sent message
+      if (Serial.available() > 0) {
+        current_state = 0;
       }
-    }
-    
-    brightnesses[i] += directions[i]*slopes[states[i]];
-    analogWrite(output_pins[i], brightnesses[i]);
+      break;
+    case 3: // message receieved
+      // stay in receive state until receiver plays message
+      if (digitalRead(message_play)) {
+        // turn off light once receiver acknowledges message
+        digitalWrite(message_available, LOW);
+        current_state = 4;
+      }
+      break;
+    case 4: // play message
+      while(Serial.read() != 0xFF){
+        continue;
+      }
+      for (int i = 0; i < totalBytesInMessage; i++) {
+        playMessage(Serial.read());
+      }
+      break;
   }
-
-  // send press_ID(s)  
-  for (int i = 0; i < num_buttons; i++) {
-    if (staging[i] == 'b') { // send press notification
-      Serial.write(press_ID[i]);
-      staging[i] = 'a';
-    }
-  }
-
-  
-  
-  delay(30);
 }
